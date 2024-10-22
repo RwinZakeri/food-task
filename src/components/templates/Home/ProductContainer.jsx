1fc0cc85d271e5746185abc7fa327efcbd5a70ae
arvin <rwiiinzakeri@gmail.com>
2024-10-22 13:12:39 +0330

    shit

--- src/components/templates/Home/ProductContainer.jsx
+++ src/components/templates/Home/ProductContainer.jsx
@@ -1,27 +1,25 @@
-import React, { Suspense } from "react";
-import axios from "axios";
+import { Suspense } from "react";
 // components
-import ProductBar from "@/components/modules/Home/ProductBar/ProductBar";
-import ProductCard from "@/components/modules/Home/ProductBar/ProductCard";
 // fetchData
-const getData = async () => {
-  const res = await fetch("http://localhost:3000/api", {
-    cache: "no-store",
-  });
-  if (!res) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-  const data = await res.json();
-  return data;
-};
+// const getData = async () => {
+//   const res = await fetch("http://localhost:3000/api", {
+//     cache: "no-store",
+//   });
+//   if (!res) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error("Failed to fetch data");
+//   }
+//   const data = await res.json();
+//   return data;
+// };
+import ProductBar from "@/components/modules/Home/ProductBar/ProductBar";
+import db from "@/db/product.json";
 async function ProductContainer() {
-  const data = await getData();
-
+  // const data = await getData();
   return (
     <div className="w-full">
       <Suspense fallback={"waiting..."}>
-        <ProductBar data={data} />
+        <ProductBar data={db} />
       </Suspense>
     </div>
   );
